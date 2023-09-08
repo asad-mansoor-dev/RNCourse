136d24117a74c40fed6db04b35b0d120d3987847
Asad Mehmood <asadpk80@gmail.com>
2023-09-08 10:42:30 -0400

    set layout with flex

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import { Colors } from "react-native/Libraries/NewAppScreen";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!"></TextInput>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your course goal!"
+          style={styles.textInput}
+        ></TextInput>
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -18,9 +21,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-  inputDirection: {
+  inputContainer: {
+    flex: 1,
     flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 2,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
